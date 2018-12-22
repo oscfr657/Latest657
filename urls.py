2b008f0de470ab418bfc75719ecb0ee108d38588
OscFr <oscfr657@gmail.com>
2018-12-22 18:48:05 +0100

    urls.py compatibility bug fix.

--- urls.py
+++ urls.py
@@ -1,13 +1,7 @@
-try:
-    from django.urls import path
-except  ImportError:
-    from django.conf.urls import url as path
 
-from rest_framework.urlpatterns import format_suffix_patterns
+from django.conf.urls import url
 from latest657 import views
 
 urlpatterns = [
-    path('^api/latestpost/', views.latest_post, name='latestpost'),
+    url('^api/latestpost/', views.latest_post, name='latestpost'),
 ]
-
-urlpatterns = format_suffix_patterns(urlpatterns)
